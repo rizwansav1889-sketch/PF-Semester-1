d30b516d89fc656fb2e5b2e478f079d0e6d404ff
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 07:48:43 -0800

    Hours to Seconds
    Convert hours input by user into seconds for time-tracking.

---
+++ Lab-Task/Lab_3/7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int h,m,s;
+  cout<<"Enter a number of Hours = ";
+  cin>>h;
+  m=60*h;
+  s=60*m;
+  cout<<h<<" Hours is equal to "<<s<<" seconds";
+
+}
