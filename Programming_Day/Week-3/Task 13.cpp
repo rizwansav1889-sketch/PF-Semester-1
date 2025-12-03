44e29f56c114bf1ac972ccc2c2ef7e7a2731b74d
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 09:23:54 -0800

    Food for Thought
    Compute sum of five integers using only two variables.

---
+++ Programming_Day/Week-3/Task 13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter number 1: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter number 2: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter number 3: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter number 4: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter number 5: ";
+    cin >> num;
+    sum += num;
+
+    cout << "The sum is: " << sum << endl;
+    return 0;
+}
