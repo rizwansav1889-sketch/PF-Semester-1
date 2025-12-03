75ab31ea00102b8896e1d5821f70cbb9216f9f93
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 08:13:59 -0800

    Buy Dress (If-Else)
    If the dress costs less than 1500, print "Buy the dress"; else, print "Do not buy the dress".

---
+++ Lab-Task/Lab_4/Ex1complete.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int cost;
+  cout<<"Enter the cost of dress = ";
+  cin>>cost;
+  if(cost<1500)
+  cout<<"Buy the Dress";
+  else
+  cout<<"Do not Buy the Dress";
+}
