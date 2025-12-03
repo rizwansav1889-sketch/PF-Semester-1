a9dd53156b4f9c315fbc08e8ae50732117779d8e
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 08:16:40 -0800

    University Attendance (If-Else)
    f input is "y", print "You are also going"; else print "You are not going".

---
+++ Lab-Task/Lab_4/Ex2complete.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char going;
+  cout<<"Are your friend is going (press Y for Yes) = ";
+  cin>>going;
+  if(going=='Y'){
+  cout<<"You are also going !";
+  }
+  else
+  cout<<"You Are not Going!";
+}
