3e81078d48f6424db94f17b47c05cc48589a88ea
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 07:41:59 -0800

    Input Personal Details
    Take user input: name, roll number, aggregate, section and print.

---
+++ Lab-Task/Lab_3/1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+    int main(){
+        int roll;
+        string name;
+        float aggrecate;
+        char session;
+        cout<<"Enter your name = ";
+        cin>>name;
+        cout<<endl<<"Enter your rool no = ";
+        cin>>roll;
+        cout<<endl<<"Enter your Aggrecate = ";
+        cin>>aggrecate;
+        cout<<endl<<"Enter your session = ";
+        cin>>session;
+        cout<<"Student Information"<<endl;
+        cout<<"Name : "<<name<<endl;
+        cout<<"Roll no : "<<roll<<endl;
+        cout<<"Aggrecate : "<<aggrecate<<endl;
+        cout<<"Session : "<<session<<endl;
+        cout<<"End";
+        
+
+
+    }
