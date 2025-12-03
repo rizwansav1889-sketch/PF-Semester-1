a8693fe7129372743ddbfaee4aead219795587a9
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 08:17:44 -0800

    Welcome Ali
    Check if the student’s name is "Ali". If yes, print "Welcome Ali"; otherwise, print "Try again".

---
+++ Lab-Task/Lab_4/Ex3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+   string name;
+   cout<<"Please Enter your Name = ";
+   cin>>name;
+   if(name=="Ali"){
+    cout<<"Congragulation Ali"<<endl;
+
+    cout<<"Welcome to UET";
+   }
+
+}
