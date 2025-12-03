7ce7efc2abde37952e5b5ee3e167585b144f8e59
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 08:18:29 -0800

    Welcome Ali (If-Else)
    If input is "Ali", print welcome message; else print "Try again".

---
+++ Lab-Task/Lab_4/Ex3complete.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+   string name;
+   cout<<"Please Enter your Name = ";
+   cin>>name;
+   if(name=="Ali"){
+    cout<<"Congragulation Ali"<<endl;
+
+    cout<<"Welcome to UET";
+   }
+   else
+   cout<<"Try Again";
+
+}
