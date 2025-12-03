efdb098b9938613f0f5c4e6d0995b930a809a9ac
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 07:50:38 -0800

    Age in Days
    Take age in years and convert to days (ignore leap years).

---
+++ Lab-Task/Lab_3/9.cpp
@@ -0,0 +1,10 @@
+ #include<iostream>
+using namespace std;
+int main(){
+   int age,days;
+   cout<<"Enter your age in years = ";
+   cin>>age;
+   days=365*age;
+   cout<<"Your age in days is approximately = ";
+   cout<<days;
+}
