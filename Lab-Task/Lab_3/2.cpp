3e0aeb7dcfad2cde4c51b068a7d9f7bb56c0b971
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 07:43:48 -0800

    Pounds to Kilograms
    Convert weight entered in pounds to kilograms using formula 1 lb = 0.45 kg.

---
+++ Lab-Task/Lab_3/2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+        int wp;
+        float wl;
+        cout<<"Enter weight in pounds = ";
+        cin>>wp;
+        wl=0.45*wp;
+        cout<<endl<<wp<<" Pounds is equal to = "<<wl;
+    }
