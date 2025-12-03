1750ea61cd08bf2b276a36c9aedba3343b694092
Rizwan Ansar <rizwansav1889@gmail.com>
2025-12-03 07:45:47 -0800

    Calculating Electric Current
    Take charge (Q) and time (t) as input and calculate current I = Q/t.

---
+++ Lab-Task/Lab_3/4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int q,t;
+    float i;
+cout<<"Enter Charge (Q) in columbs = ";
+cin>>q;
+cout<<endl<<"Enter Time in seconds = ";
+cin>>t;
+i=q/t;
+cout<<"The current = "<<i;
+
+
+}
